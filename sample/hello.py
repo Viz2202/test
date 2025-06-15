625c9fe3f006054ddb25aa8b8fe2130033cd922f
Viz2202 <vishnurejeesh22@gmail.com>
2025-06-15 11:29:02 +0530

    223

--- sample/hello.py
+++ sample/hello.py
@@ -1,3 +1,3 @@
 for i in range(5):
-    print("Hello, World!")
+    printyuyu("Hello, World!")
     print(i)
--- sample/hello_again.py
+++ sample/hello_again.py
@@ -1,4 +1,4 @@
 def hello():
-    printttt("Hello again!")
+    print("Hello again!")
 
 hello()
