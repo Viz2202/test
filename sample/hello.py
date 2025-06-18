e2272f416cec32e5b74ec398bf1a77d5ac44d93f
Viz2202 <vishnurejeesh22@gmail.com>
2025-06-18 22:59:40 +0530

    3254334

--- sample/hello.py
+++ sample/hello.py
@@ -1,3 +1,17 @@
-for i in range(5):
-    printii("Baadshah")
-    print(i)
+# for i in range(5):
+#     printii("Baadshah")
+#     print(i)
+
+def quicksort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[len(arr) // 2]
+    left = [x for x in arr if x <= pivot] 
+    middle = [x for x in arr if x == pivot]
+    right = [x for x in arr if x > pivot]
+    return quicksort(left) + middle + quicksort(right)
+
+# Example usage
+arr = [3, 6, 8, 10, 1, 2, 1]
+print(quicksort(arr))
+
