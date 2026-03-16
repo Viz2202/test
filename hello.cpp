d716828156ede74342d49aa5d1f2f413090a7f82
Viz2202 <vishnurejeesh22@gmail.com>
2026-03-16 19:47:05 +0530

    changes to hello

--- hello.cpp
+++ hello.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
-using namespace std;
-int main(){
-    cout<<"Hello World";
-    return 0;
+using namespace std
+
+int main() {
+    int a = 10
+    int b = 20;
+
+    if(a < b) {
+        cout << "a is smaller than b" << endl
+    }
+
+    for(int i = 0; i < 5; i++) {
+        cout << i << endl;
+    }
+
+    int arr[5] = {1,2,3,4,5
+
+    return 0
 }
